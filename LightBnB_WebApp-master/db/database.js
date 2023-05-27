360e85c640d3bc20cf684457de9318378cbe75eb
Matt Rostad <matt@rostad.ca>
2023-05-26 17:26:09 -0700

    update addProperty

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -187,11 +187,51 @@ getAllProperties(options, 10).then((rows) => {
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
+
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return new Promise((resolve, reject) => {
+    // Define the SQL query to insert the property into the table
+    const query = `
+      INSERT INTO properties (
+        owner_id, title, description, thumbnail_photo_url, cover_photo_url,
+        cost_per_night, street, city, province, post_code, country,
+        parking_spaces, number_of_bathrooms, number_of_bedrooms
+      )
+      VALUES (
+        $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14
+      )
+      RETURNING *;
+    `;
+
+    // Extract the property values into an array
+    const values = [
+      property.owner_id,
+      property.title,
+      property.description,
+      property.thumbnail_photo_url,
+      property.cover_photo_url,
+      property.cost_per_night,
+      property.street,
+      property.city,
+      property.province,
+      property.post_code,
+      property.country,
+      property.parking_spaces,
+      property.number_of_bathrooms,
+      property.number_of_bedrooms
+    ];
+
+    // Execute the query using the connection pool
+    pool.query(query, values)
+      .then(result => {
+        // Resolve the promise with the saved property
+        resolve(result.rows[0]);
+      })
+      .catch(error => {
+        // Reject the promise with the error
+        reject(error);
+      });
+  });
 };
 
 module.exports = {
